9a0233cf3bbf08211230878fdb0a4788125393b1
francisnguyenanh <onlyone231287@gmail.com>
2026-03-29 12:36:57 +0900

    update

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 SECRET_KEY = 'shadowing-local-2024'
-DATABASE = 'database.db'
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE = os.path.join(BASE_DIR, 'database.db')
+
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
